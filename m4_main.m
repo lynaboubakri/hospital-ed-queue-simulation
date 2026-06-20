cf22931ee36a0d6cc265a63fa1f1f45d066c40e2
alyabatrisyiia <168154978+alyabatrisyiia@users.noreply.github.com>
2026-06-20 14:50:13 +0800

    Update m4_main.m

--- m4_main.m
+++ m4_main.m
@@ -1,1 +1,11 @@
+% ==================================================
+% main.m 
+% Member 4 - Main Simulation Controller
+% Hospital Emergency Department Simulation
+% ==================================================
+
+clear all;
+clc;
+close all;
+
 
